059f4adb77cbf1c90af566048c30688f808fbbc4
aliasgar1978 <aholo2000@gmail.com>
2021-05-31 09:31:03 +0530

    minor updates

--- compare_it/__init__.py
+++ compare_it/__init__.py
@@ -2,6 +2,6 @@ __doc__ = '''Compare IT Utility'''
 
 __all__ = [ "CompareText", "CompareExcelData" ]
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from .diff import CompareText, CompareExcelData, get_string_diffs
