6189a460784114dbf1079b7912b8846a288b9055
aliasgar1978 <aholo2000@gmail.com>
2022-02-19 10:07:32 +0530

    .

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.autosummary',
     'sphinx_rtd_theme',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
